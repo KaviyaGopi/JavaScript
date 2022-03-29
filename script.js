1d98a4c752dc20c21fb6687f7bd521ee01bff632
KaviyaGopi <kaviyagopi002@gmail.com>
2022-03-29 22:08:18 +0530

    Values and Variables

---
+++ script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Kavya");
+console.log(23);
+
+let firstName = "Kavya";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let kavya_gopi = "KG";
+let $function = 27;
+
+let person = "Kavya";
+let PI = 3.1415;
+
+let muGirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
