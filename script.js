e58a43fb043b0f39a28381b6c1fce176c31369c2
KaviyaGopi <kaviyagopi002@gmail.com>
2022-03-30 06:40:09 +0530

    Data Types

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Kavya");
-console.log(23);
+// console.log("Kavya");
+// console.log(23);
 
-let firstName = "Kavya";
+// let firstName = "Kavya";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let kavya_gopi = "KG";
-let $function = 27;
+// let kavya_gopi = "KG";
+// let $function = 27;
 
-let person = "Kavya";
-let PI = 3.1415;
+// let person = "Kavya";
+// let PI = 3.1415;
 
-let muGirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let muGirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "Teacher";
+// let job1 = "programmer";
+// let job2 = "Teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Kavya");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(tyoeof null);
