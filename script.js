de30d7e4808b637d5595a51f2e1aa70ea95a5092
KaviyaGopi <kaviyagopi002@gmail.com>
2022-03-30 06:53:28 +0530

    let, const and var

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-// let js = "amazing";
-// console.log(40 + 8 + 23 - 10);
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-// console.log("Kavya");
-// console.log(23);
+console.log("Kavya");
+console.log(23);
 
-// let firstName = "Kavya";
+let firstName = "Kavya";
 
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
-// let kavya_gopi = "KG";
-// let $function = 27;
+let kavya_gopi = "KG";
+let $function = 27;
 
-// let person = "Kavya";
-// let PI = 3.1415;
+let person = "Kavya";
+let PI = 3.1415;
 
-// let muGirstJob = "Programmer";
-// let myCurrentJob = "Teacher";
+let muGirstJob = "Programmer";
+let myCurrentJob = "Teacher";
 
-// let job1 = "programmer";
-// let job2 = "Teacher";
+let job1 = "programmer";
+let job2 = "Teacher";
 
-// console.log(myFirstJob);
+console.log(myFirstJob);
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
-// console.log(typeof true);
+console.log(typeof true);
 console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof "Kavya");
+console.log(typeof 23);
+console.log(typeof "Kavya");
 
 javascriptIsFun = "YES!";
 console.log(javascriptIsFun);
@@ -41,4 +41,20 @@ console.log(typeof year);
 
 year = 1991;
 
-console.log(tyoeof null);
+onsole.log(tyoeof null);
+*/
+
+let age = 30;
+age = 31;
+console.log(age);
+
+// const cannot be changed .
+const birthYear = 2002;
+// birthYear = 1999;
+// const job;
+
+var job = "Programmer";
+job = "Teacher";
+
+lastName = "Gopi";
+console.log(lastName);
