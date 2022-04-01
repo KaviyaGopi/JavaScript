5747eae4f0f1a6dff5d4ca1230e46e84ad70ff27
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-01 15:40:26 +0530

    Challenge 4

--- script.js
+++ script.js
@@ -71,9 +71,9 @@ const firstName = "Kavya";
 const lastName = "Gopi";
 console.log(firstName + " " + lastName);
 
------------------------
+--------------------
 Assignment Operator 
-------------------------
+-------------------
 
 let x = 10 + 5; // 15
 x += 10; // x = x + 10
@@ -82,9 +82,9 @@ x++; // x = x + 1
 x--; // x = x - 1
 console.log(x);
 
------------------------
+--------------------
 Comparison Operator
------------------------
+--------------------
 
 console.log(ageKavya < ageMadhu); // >, <, >=. <=
 console.log(ageMadhu >= 18);
@@ -110,9 +110,9 @@ const averageAge = ageKavya + ageMadhu / 2
 console.log(ageKavya, ageMadhu, averageAge)
 
 
-----------------------------------------------------------------------------
-
+--------------------
 Coding Challenge #1
+--------------------
 
 Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
 BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
@@ -346,9 +346,10 @@ if (hasDriverLicense && hasGoodVision && isTired) {
   console.log("someone else should drive...");
 }
 
----------------------------------------------------------------------------------------
+--------------------
 Coding Challenge #3
 -------------------
+
 There are two gymnastics teams, Dolphins and Koalas. They compete against each
 other 3 times. The winner with the highest average score wins a trophy!
 Your tasks:
@@ -375,7 +376,7 @@ Test Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
 Test Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
 GOOD LUCK 😀
---------------------------------------------------------------------------------------
+-------------------------------------------------------
 
 const scoreDolphins = (96 + 108 + 89) / 3;
 const scoreKoales = (88 + 91 + 110) / 3;
@@ -395,7 +396,7 @@ if (scoreDolphins > scoreKoales && scoreDolphins >= 100) {
   console.log("No one wins the trophy");
 }
 
-------------------------------------------------------------------------------------------
+---------------------------------------------------
 
 const day = "Monday";
 
@@ -422,7 +423,7 @@ switch (day) {
     console.log("Not a valid day!");
   }
   
-  -----------------------------------------------------------------------------------------
+  ------------------------------------------------
 
   if (day === "Monday") {
     console.log("Plan course structure");
@@ -439,23 +440,22 @@ switch (day) {
     console.log("Not a valid day!");
   }
   
-  -----------------------------------------------------------------------------------------
+  -----------------------------------
   The Conditional (Ternary) Operator
   -----------------------------------
-  */
-
-const age = 23;
-if (age >= 18) {
-  console.log("I like to drink wine");
-} else {
-  console.log("I like to drink water");
-}
-
-const drink = age >= 18 ? "wine" : "water";
-console.log(drink);
-
-let drink2;
-if (age >= 18) {
+  
+  const age = 23;
+  if (age >= 18) {
+    console.log("I like to drink wine");
+  } else {
+    console.log("I like to drink water");
+  }
+  
+  const drink = age >= 18 ? "wine" : "water";
+  console.log(drink);
+  
+  let drink2;
+  if (age >= 18) {
   drink2 = "wine";
 } else {
   drink2 = "water";
@@ -463,3 +463,41 @@ if (age >= 18) {
 console.log(drink2);
 
 console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+
+--------------------
+Coding Challenge #4
+--------------------
+
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+
+Your tasks:
+-----------
+
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+
+Test data:
+
+Data 1: Test for bill values 275, 40 and 430
+Hints:
+To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+
+*/
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
