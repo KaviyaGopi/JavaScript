5d1a8814b7c5b1c6b8e9996e5b9073b50b366faa
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-01 19:43:08 +0530

    Functions

--- Fundamentals Part-02/script.js
+++ Fundamentals Part-02/script.js
@@ -1,3 +1,4 @@
+/*
 "use strict";
 
 let hasDriversLicense = false;
@@ -6,5 +7,27 @@ let passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive :D  ");
 
-// const interface = "Audio";
-// const private = 534;
+const interface = "Audio";
+const private = 534;
+*/
+
+function logger() {
+  console.log("my name is Kavya");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
