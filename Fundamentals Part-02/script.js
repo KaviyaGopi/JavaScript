452a8098cdcfc60eb051eb74ea92dcabe98ed5a4
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-02 20:18:00 +0530

    Function Declarations vs. Expressions

--- Fundamentals Part-02/script.js
+++ Fundamentals Part-02/script.js
@@ -9,7 +9,6 @@ if (hasDriversLicense) console.log("I can drive :D  ");
 
 const interface = "Audio";
 const private = 534;
-*/
 
 function logger() {
   console.log("my name is Kavya");
@@ -31,3 +30,22 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+------------------------------------
+Function Declaration vs Expressions
+------------------------------------
+*/
+
+// Function Declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
