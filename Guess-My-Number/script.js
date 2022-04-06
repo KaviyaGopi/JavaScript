5e0cc84c0c489968b43e825371b1be456383f43a
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-06 20:42:04 +0530

    Selecting and Manipulating Elements

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.number').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
