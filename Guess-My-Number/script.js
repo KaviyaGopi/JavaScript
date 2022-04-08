f25924d2e9f4c5bd38f4125988484c1449a5bcf7
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-08 17:46:32 +0530

    Handling Click Event

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,11 +1,25 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number';
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.number').textContent = 13;
-document.querySelector('.number').textContent = 20;
+document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(',message').textContent = 'No Number!';
+  }
+});
