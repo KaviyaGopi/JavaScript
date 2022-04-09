cc378a5db1b15b71b865494437088303ad6efd2b
KaviyaGopi <kaviyagopi002@gmail.com>
2022-04-09 15:50:12 +0530

    Guess the number

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -10,16 +10,58 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+'use strict';
 
-const x = function () {
-  console.log(23);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const checkTheScore = (msg, status = 'playing', color) => {
+  document.querySelector('.message').textContent = msg;
+  if (status !== 'playing') {
+    // it's making inline style and not changing the css file!
+    document.querySelector('body').style.backgroundColor = color;
+  }
+  if (status === 'win') {
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (status === 'lose') {
+    document.querySelector('.score').textContent = 0;
+  } else if (status === 'playing') {
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 };
 
 document.querySelector('.check').addEventListener('click', function () {
+  document.querySelector('.score').textContent = score;
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
-  if (!guess) {
-    document.querySelector(',message').textContent = 'No Number!';
+  if (score > 1) {
+    if (guess < 0 || guess > 20) {
+      checkTheScore('Enter number between 0-20');
+    } else if (guess === secretNumber) {
+      checkTheScore('Correct Number', 'win', '#60b347');
+    } else if (guess !== secretNumber)
+      guess < secretNumber
+        ? checkTheScore('Too Low!')
+        : checkTheScore('Too high!');
+  } else {
+    checkTheScore('You lost the game!', 'lose', '#ff0000');
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  console.log('here');
 });
